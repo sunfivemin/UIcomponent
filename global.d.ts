27f0f944439997a505cfe75785af697ceab3b734
sunfivemin <154585975+sunfivemin@users.noreply.github.com>
2025-07-15 22:29:43 +0900

    Fix JSX namespace with global declaration

--- global.d.ts
+++ global.d.ts
@@ -3,7 +3,9 @@ declare module "*.md";
 /// <reference types="react" />
 /// <reference types="react-dom" />
 
-declare namespace JSX {
-  interface Element extends React.ReactElement<any, any> {}
-  interface IntrinsicElements extends React.JSX.IntrinsicElements {}
+declare global {
+  namespace JSX {
+    interface Element extends React.ReactElement<any, any> {}
+    interface IntrinsicElements extends React.JSX.IntrinsicElements {}
+  }
 }
